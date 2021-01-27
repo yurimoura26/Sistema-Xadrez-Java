5a77fd7041eda71e6a505ad46ff48cac9b1f829a
Yuri Cardoso Moura <phowkyx.1990@gmail.com>
2021-01-27 11:46:18 -0300

    Classe Pincipal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,21 @@
 package application;
 
+import java.util.Scanner;
+
+import tabuleiro.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("aaaa");
+		Scanner ler = new Scanner(System.in);
+		
+		
+		
+		
+		Position p = new Position(3, 5);
+		System.out.println(p );
+		
+	ler.close();	
 	}
 
 }
