9e173f62d5ef6128946e8f4d625385780c3a3027
Yuri Cardoso Moura <phowkyx.1990@gmail.com>
2021-01-27 21:10:40 -0300

    Classe Exceção

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,12 +1,15 @@
 package tabuleiro;
 
 public class Tabuleiro {
-	
+		
 		private int linhas;
 		private int colunas;
 		private Peca[][] pecas;
 		
 		public Tabuleiro(int linhas, int colunas) {
+			if (linhas < 1 || colunas < 1) {
+				throw new TabuleiroExceprion("Erro: tabuleiro não jogavel");
+			}
 			this.linhas = linhas;
 			this.colunas = colunas;
 			pecas = new Peca[linhas][colunas];
@@ -16,26 +19,45 @@ public class Tabuleiro {
 			return linhas;
 		}
 
-		public void setLinhas(int linhas) {
-			this.linhas = linhas;
-		}
-
 		public int getColunas() {
 			return colunas;
 		}
-
-		public void setColunas(int colunas) {
-			this.colunas = colunas;
-		}
 		
 		public Peca peca(int linha, int coluna) {
+			if (!positionExiste(linha ,coluna)) {
+				throw new TabuleiroExceprion("Erro: Posição não Existe no tabuleiro");
+			}
 			return pecas[linha][coluna];
 		}
+		
 		public Peca peca(Position posicao) {
+			if (!positionExiste(posicao)) {
+				throw new TabuleiroExceprion("Erro: já existe uma peça na posição ");
+			}
 			return pecas[posicao.getLinha()][posicao.getColuna()];
 		}
+		
 		public void placePeca(Peca peca, Position posicao) {
+			if (temPecaPosicao(posicao)) {
+				throw new TabuleiroExceprion("Erro: já existe uma peça na posição "+posicao);
+			}
 			pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 			peca.posicao = posicao;
 		}
+		
+		private boolean positionExiste(int linha, int coluna) {
+			return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+		}
+		
+		public boolean positionExiste(Position posicao) {
+			return positionExiste(posicao.getLinha(), posicao.getColuna());
+		}
+		
+		public boolean temPecaPosicao(Position posicao) {
+			if (!positionExiste(posicao)) {
+				throw new TabuleiroExceprion("Erro: Posição não Existe no tabuleiro ");
+			}
+			return peca(posicao) !=null;
+		}
+
 }
---
+++ src/tabuleiro/TabuleiroExceprion.java
@@ -0,0 +1,10 @@
+package tabuleiro;
+
+public class TabuleiroExceprion extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public TabuleiroExceprion (String msg) {
+		super(msg);
+	}
+}
